c2adb5486715cdecd4106126200706cec1648ce9
DrMothra <tony.glover@nottingham.ac.uk>
2017-01-17 23:55:25 +0000

    Generate graph from client, UI update

--- routes/graphs.js
+++ routes/graphs.js
@@ -2,7 +2,23 @@
  * Created by atg on 16/01/2017.
  */
 
+var parser = require("../model/parser");
+
+function convertToJSON(csvData) {
+    var parseOutput = parser.parse(csvData, true , "auto", false, false);
+
+    var dataGrid = parseOutput.dataGrid;
+    var headerNames = parseOutput.headerNames;
+    var headerTypes = parseOutput.headerTypes;
+    var errors = parseOutput.errors;
+
+    var outputText = parser.dataGridRenderer(dataGrid, headerNames, headerTypes, "  ", "\n");
+
+    return outputText;
+}
+
 //var currentGraph = require("../model/currentGraph.js");
+
 var gc = require("graphcommons");
 var accesskey = process.env.GRAPH_COMMONS_API_KEY;
 var graphCommons = new gc(accesskey, function(result) {
@@ -26,10 +42,20 @@ exports.generateGraph = function(req, res, next) {
 };
 
 exports.processUpload = function(req, res, next) {
-    console.log("File uploaded = ", req.files);
-    var temp = req.files.vizFile.data.toString();
-    var vizDataFile = JSON.parse(temp);
-    console.log("New json file created");
+    var fileName = req.files.vizFile.name;
+    var fileData = req.files.vizFile.data;
+    fileData = fileData.toString();
 
+    //DEBUG
+    //console.log("File = ", fileName);
+
+    if(fileName.slice(-4) === ".csv") {
+        fileData = convertToJSON(fileData);
+    } else {
+        fileData = JSON.parse(fileData);
+    }
+
+    //DEBUG
+    console.log("Output = ", fileData);
     res.end();
 };
