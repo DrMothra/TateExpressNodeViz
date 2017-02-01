db264b9824c9b349723291fce31212221f1602ac
DrMothra <tony.glover@nottingham.ac.uk>
2017-02-01 17:39:16 +0000

    Static public files

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var io = require('socket.io')(server);
  */
 
 var address = process.env.NODE_SERVER_ADDRESS || '127.0.0.1';
-server.listen(port, address);
+server.listen(port);
 //server.listen(port, '127.0.0.1');
 server.on('error', onError);
 server.on('listening', onListening);
@@ -73,7 +73,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(fileUpload());
 
 app.use('/', index);
